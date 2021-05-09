114857db2af2fcf80c2c024d955eff5b8c7dccae
saga6569 <aristov6569@gmail.com>
2021-05-09 10:49:18 +0200

    step 1.6

---
+++ index.js
@@ -0,0 +1,4 @@
+import component from './src/index.js';
+
+// eslint-disable-next-line no-undef
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
 };
