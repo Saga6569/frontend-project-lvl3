34197abfc633406c2c39d4445d6291ee127c4382
saga6569 <aristov6569@gmail.com>
2021-08-26 18:27:33 +0200

    step 4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import onChange from 'on-change';
 import * as yup from 'yup';
 import { setLocale } from 'yup';
+import axios from 'axios';
 import Example from './Example.js';
 import render from './render';
 
@@ -33,6 +34,26 @@ export default () => {
   };
 
   const watchedState = onChange(state, (path, value) => {
+    const parser = new DOMParser();
+    axios.get(value)
+      .then((response) => {
+        const xmlString = response.data;
+        console.log(xmlString);
+        const doc1 = parser.parseFromString(xmlString, 'text/html');
+        console.log(doc1.querySelector('channel'));
+        const html = doc1.querySelector('channel');
+        //console.log(html.children);
+        console.log(doc1);
+        const cild = Array.from(html.children);
+        const f = cild.filter((el) => el.tagName.toLowerCase() === 'item');
+        console.log(f);
+        // const items = (html.children).filter(el => el.innerHTML === 'item');
+        // console.log(items);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
     const promise = schema.validate({ url: value });
     promise
       .then(() => {
