f98f2686c24b4e956b8fc2e781ef4339bab164ab
Saga6569 <aristov6569@gmail.com>
2021-10-04 20:29:36 +0500

    refactoring 1.4

--- src/utilits.js
+++ src/utilits.js
@@ -29,6 +29,7 @@ export const generationData = (html) => {
 export const parserData = (data) => {
   const parser = new DOMParser();
   const result = parser.parseFromString(data, 'application/xml');
+  console.log(result);
   return result.activeElement.nodeName === 'parsererror' ? 'er' : result;
 };
 
